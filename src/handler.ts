9ac34d7d355a4cbd8416eed695a4256be785ef90
Matthew Blewitt <blewitt.matt@gmail.com>
2024-10-19 21:44:44 +0100

    error handle the validation

--- src/handler.ts
+++ src/handler.ts
@@ -1,9 +1,16 @@
-import { createHandler } from "graphql-http/lib/use/express";
+import {
+  createHandler,
+  OperationContext,
+  Request as RawRequest,
+} from "graphql-http";
+import { HandlerOptions, RequestContext } from "graphql-http/lib/use/express";
 import {
   parse,
   specifiedRules,
+  GraphQLError,
   NoSchemaIntrospectionCustomRule,
 } from "graphql";
+import { Handler, Request, Response } from "express";
 import { maxAliasesRule } from "@escape.tech/graphql-armor-max-aliases";
 import { maxDirectivesRule } from "@escape.tech/graphql-armor-max-directives";
 import { maxDepthRule } from "@escape.tech/graphql-armor-max-depth";
@@ -13,7 +20,54 @@ import { schema } from "./schema";
 import { limitFieldsRule } from "./rules";
 import { config } from "./config";
 
-const graphQLHandler = createHandler({
+const toRequest = (
+  req: Request,
+  res: Response
+): RawRequest<Request, RequestContext> => {
+  return {
+    url: req.url,
+    method: req.method,
+    headers: req.headers,
+    body: () => {
+      if (req.body) {
+        return req.body;
+      }
+      return new Promise<string>((resolve) => {
+        let body = "";
+        req.setEncoding("utf-8");
+        req.on("data", (chunk) => (body += chunk));
+        req.on("end", () => resolve(body));
+      });
+    },
+    raw: req,
+    context: { res },
+  };
+};
+
+const handleFunc = <Context extends OperationContext = undefined>(
+  options: HandlerOptions<Context>
+): Handler => {
+  const handle = createHandler(options);
+  return async (req, res) => {
+    try {
+      const [body, init] = await handle(toRequest(req, res));
+      res.writeHead(init.status, init.statusText, init.headers).end(body);
+    } catch (err) {
+      if (err instanceof GraphQLError) {
+        res
+          .status(400)
+          .json({ errors: [err] })
+          .end();
+
+        return;
+      }
+
+      res.writeHead(500).end();
+    }
+  };
+};
+
+const graphQLHandler = handleFunc({
   schema,
   parse: (query) => parse(query, { maxTokens: config.graphqlMaxTokens }),
   validationRules: [
--- test/app.test.ts
+++ test/app.test.ts
@@ -10,4 +10,42 @@ describe("app", () => {
     expect(response.status).toBe(200);
     expect(response.body).toEqual({ data: { __typename: "ListWidgets" } });
   });
+
+  it("should return errors when introspecting schema", async () => {
+    const response = await request(app)
+      .post("/graphql")
+      .send({ query: "{ __schema { types { name } } }" });
+    expect(response.status).toBe(200);
+    expect(response.body.errors).toHaveLength(2);
+    expect(response.body.errors[0].message).toMatch(
+      /GraphQL introspection has been disabled/
+    );
+  });
+
+  it("should return a 400 when exceeding the field limit", async () => {
+    const response = await request(app)
+      .post("/graphql")
+      .send({ query: "{ a a a }" });
+    expect(response.status).toBe(400);
+    expect(response.body.errors).toHaveLength(1);
+    expect(response.body.errors[0].message).toMatch(/Query validation error/);
+  });
+
+  it("should return a 400 when exceeding the alias limit", async () => {
+    const response = await request(app)
+      .post("/graphql")
+      .send({ query: "{ a: a b: b c: c d: d e: e f: f }" });
+    expect(response.status).toBe(400);
+    expect(response.body.errors).toHaveLength(1);
+    expect(response.body.errors[0].message).toMatch(/Query validation error/);
+  });
+
+  it("should return a 400 when exceeding the directives limit", async () => {
+    const response = await request(app)
+      .post("/graphql")
+      .send({ query: "{ __typename @a@a@a@a@a@a@a@a }" });
+    expect(response.status).toBe(400);
+    expect(response.body.errors).toHaveLength(1);
+    expect(response.body.errors[0].message).toMatch(/Query validation error/);
+  });
 });
