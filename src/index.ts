df1bd831592d255edecda1f487433ed672cb608c
Matthew Blewitt <blewitt.matt@gmail.com>
2024-10-19 12:21:05 +0100

    add initial guards

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,33 @@
-import express from "express"
-import morgan from "morgan"
-import { createHandler } from "graphql-http/lib/use/express"
-import { schema } from "./schema"
-import bodyParser from "body-parser"
+import express from "express";
+import morgan from "morgan";
+import { createHandler } from "graphql-http/lib/use/express";
+import { parse, NoSchemaIntrospectionCustomRule } from "graphql";
+import bodyParser from "body-parser";
+import { maxAliasesRule } from "@escape.tech/graphql-armor-max-aliases";
+import { maxDirectivesRule } from "@escape.tech/graphql-armor-max-directives";
 
-const root = {
-    hello: () => "Hello, world!"
-}
+import { schema } from "./schema";
 
-const app = express()
+const app = express();
 
-app.use(morgan("common"))
-app.use(bodyParser.json({ limit: "64kb" }))
-app.use(bodyParser.urlencoded({ extended: true, limit: "64kb" }))
-app.use("/graphql", createHandler({ schema, rootValue: root }))
-app.listen(3000, () => console.log("Server running on port 3000"))
+const handler = createHandler({
+  schema,
+  parse: (query) => parse(query, { maxTokens: 5000 }),
+  validationRules: [
+    NoSchemaIntrospectionCustomRule,
+    maxDirectivesRule({
+      n: 5,
+      exposeLimits: false,
+    }),
+    maxAliasesRule({
+      n: 5,
+      exposeLimits: false,
+    }),
+  ],
+});
+
+app.use(morgan("common"));
+app.use(bodyParser.json({ limit: "64kb" }));
+app.use(bodyParser.urlencoded({ extended: true, limit: "64kb" }));
+app.post("/graphql", handler);
+app.listen(3000, () => console.log("Server running on port 3000"));
